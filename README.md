708e62724ee4a438b0f130eaaad5ed09879a9339
neelkamath <neelkamath@icloud.com>
2019-09-04 18:41:25 +0530

    Fix JabbaScript

--- README.md
+++ README.md
@@ -17,18 +17,15 @@ Open `neelkamath.github.io/index.html` in your browser.
 - Use `snake_case` to name files and directories.
 - Limit lines to 120 characters except where impractical.
 
-### Web Components
-
 Browsers seem to implement the shadow DOM differently. In Safari, stylesheets located on other websites work fine when loaded only in the shadow DOM. However, in Chrome, the stylesheet must be in the HTML's head tag as well as the Shadow DOM. Hence, stylesheets used only in a shadow DOM are present globally as well.
 
-#### Project: `project-data`
+There is a single web component, `project-data`, meant for showcasing projects. Place its HTML description (mandatory) (e.g., `<a href='https://play.google.com/store/apps/details?id=io.github.neelkamath.timebend'>Play Store</a> productivity app`) in between this custom element's HTML tags. `project-data`'s attributes are given in the table below.
 
 |Attribute|Explanation|Example|Optional|
 |:-------:|-----------|-------|:------:|
-|`link`|URL|`https://github.com/neelkamath/time-bend-android`|No|
+|`link`|GitHub Repository URL|`https://github.com/neelkamath/time-bend-android`|No|
 |`name`|Name|`Time Bend`|No|
-|`icons`|Icons from [DEVICON](https://konpa.github.io/devicon/) of technologies used (listed in reverse order of appearance)|`<i class='devicon-java-plain'></i> <i class='devicon-android-plain'></i>`|Yes|
-|`description`|HTML description|`<a href='https://play.google.com/store/apps/details?id=io.github.neelkamath.timebend'>Play Store</a> productivity app`|No|
+|`icons`|[DEVICON](https://konpa.github.io/devicon/) icons' CSS class names of the technologies used (listed in reverse order of appearance)|`devicon-java-plain devicon-android-plain`|Yes|
 |`img`|Screenshot source|`src/media/projects/time_bend.png`|Yes|
 
 ## Credits
--- src/index.js
+++ src/index.js
@@ -2,25 +2,31 @@ customElements.define('project-data', class extends HTMLElement {
     constructor() {
         super();
         const shadow = this.attachShadow({mode: 'open'});
-        shadow.append(this._getStyleLink(), this._getIconLink(), this._getProjectDiv(), this._getParagraph());
+        shadow.append(this._getStyleLink(), this._getIconLink(), this._getHeading(), this._getDescription());
         if (this.hasAttribute('img')) shadow.append(this._getImage());
     }
 
+    _getDescription() {
+        const p = document.createElement('p');
+        p.append(...this.childNodes);
+        return p;
+    }
+
     _getStyleLink() {
-        const styleLink = document.createElement('link');
-        styleLink.href = 'src/styles/project_data.css';
-        styleLink.rel = 'stylesheet';
-        return styleLink;
+        const link = document.createElement('link');
+        link.href = 'src/styles/project_data.css';
+        link.rel = 'stylesheet';
+        return link;
     }
 
     _getIconLink() {
-        const iconLink = document.createElement('link');
-        iconLink.href = 'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css';
-        iconLink.rel = 'stylesheet';
-        return iconLink;
+        const link = document.createElement('link');
+        link.href = 'https://cdn.rawgit.com/konpa/devicon/df6431e323547add1b4cf45992913f15286456d3/devicon.min.css';
+        link.rel = 'stylesheet';
+        return link;
     }
 
-    _getProjectDiv() {
+    _getHeading() {
         const div = document.createElement('div');
         div.className = 'project';
         const a = document.createElement('a');
@@ -29,16 +35,12 @@ customElements.define('project-data', class extends HTMLElement {
         div.appendChild(a);
         if (this.hasAttribute('icons')) {
             const span = document.createElement('span');
-            span.innerHTML = this.getAttribute('icons');
+            for (const className of this.getAttribute('icons').split(' ')) {
+                span.innerHTML += `<i class="${className}"></i>`;
+            }
             div.appendChild(span);
         }
         return div;
-    }
-
-    _getParagraph() {
-        const p = document.createElement('p');
-        p.innerHTML = this.getAttribute('description');
-        return p;
     }
 
     _getImage() {
